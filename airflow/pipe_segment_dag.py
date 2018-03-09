66decaab90358d32a09c7863d3b3cdc00e9dff2b
Paul Woods <pwoods25443@gmail.com>
2018-03-09 13:20:22 -0500

    support directrunner for dataflow job

--- airflow/pipe_segment_dag.py
+++ airflow/pipe_segment_dag.py
@@ -1,13 +1,17 @@
 from datetime import datetime, timedelta
 import posixpath as pp
-import os
+import six
+import re
 
 from airflow import DAG
 from airflow.contrib.operators.dataflow_operator import DataFlowPythonOperator
+from airflow.contrib.operators.dataflow_operator import GoogleCloudBucketHelper
+from airflow.contrib.hooks.gcp_dataflow_hook import DataFlowHook
+from airflow.contrib.hooks.gcp_dataflow_hook import _Dataflow
 from airflow.contrib.sensors.bigquery_sensor import BigQueryTableSensor
-from airflow.contrib.operators.bigquery_operator import BigQueryOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.models import Variable
+from airflow.utils.decorators import apply_defaults
 
 
 CONNECTION_ID = 'google_cloud_default'
@@ -57,6 +61,81 @@ def table_sensor(dataset, table, date):
     )
 
 
+class DataFlowDirectRunnerHook(DataFlowHook):
+    def _start_dataflow(self, task_id, variables, dataflow, name, command_prefix):
+        cmd = command_prefix + self._build_cmd(task_id, variables, dataflow)
+        _Dataflow(cmd).wait_for_done()
+
+    def _build_cmd(self, task_id, variables, dataflow):
+        command = [dataflow]
+        if variables is not None:
+            for attr, value in variables.iteritems():
+                command.append("--" + attr + "=" + value)
+        return command
+
+class DataFlowOperator(DataFlowPythonOperator):
+    def execute_direct_runner(self, context):
+        bucket_helper = GoogleCloudBucketHelper(
+            self.gcp_conn_id, self.delegate_to)
+        self.py_file = bucket_helper.google_cloud_to_local(self.py_file)
+        dataflow_options = self.dataflow_default_options.copy()
+        dataflow_options.update(self.options)
+        # Convert argument names from lowerCamelCase to snake case.
+        camel_to_snake = lambda name: re.sub(
+            r'[A-Z]', lambda x: '_' + x.group(0).lower(), name)
+        formatted_options = {camel_to_snake(key): dataflow_options[key]
+                             for key in dataflow_options}
+        hook = DataFlowDirectRunnerHook(gcp_conn_id=self.gcp_conn_id,
+                            delegate_to=self.delegate_to)
+        hook.start_python_dataflow(
+            self.task_id, formatted_options,
+            self.py_file, self.py_options)
+
+        pass
+
+    def execute(self, context):
+        """Execute the python dataflow job."""
+        if self.options['runner'] == 'DirectRunner':
+            return self.execute_direct_runner(context=context)
+        else:
+            return super(DataFlowOperator, self).execute(context=context)
+
+
+
+
+#
+# def dataflowOperator(task_id, options, **kwargs):
+#     if options['runner'] == 'DirectRunner':
+#         return BashOperator(
+#             task_id=task_id,
+#             bash_command='',
+#             **kwargs
+#         )
+#
+#         identity_messages_monthly = BashOperator(
+#             task_id='identity_messages_monthly',
+#             pool='bigquery',
+#             bash_command='{docker_run} {docker_image} identity_messages_monthly '
+#                          '{project_id}:{pipeline_dataset}.{messages_table} '
+#                          '{project_id}:{pipeline_dataset}.{identity_messages_monthly_table}{first_day_of_month_nodash} '
+#                          '{first_day_of_month} {last_day_of_month}'.format(**config)
+#         )
+#
+#     else:
+#         return DataFlowPythonOperator(
+#             task_id=task_id,
+#             options=options,
+#             **kwargs
+#         )
+#
+#             pool='dataflow',
+#             depends_on_past=True,
+#             py_file=Variable.get('DATAFLOW_WRAPPER_STUB'),
+#             priority_weight=10,
+#             options=dict(
+# ):
+
+
 def build_dag(dag_id, schedule_interval='@daily', extra_default_args=None, extra_config=None):
 
     default_args=DEFAULT_ARGS.copy()
@@ -82,7 +161,7 @@ def build_dag(dag_id, schedule_interval='@daily', extra_default_args=None, extra
         source_sensors = [table_sensor(dataset, table, source_sensor_date) for table in source_tables]
         source_paths = ['bq://{}:{}.{}'.format(project, dataset, table) for table in source_tables]
 
-        segment = DataFlowPythonOperator(
+        segment = DataFlowOperator(
             task_id='segment',
             pool='dataflow',
             depends_on_past=True,
@@ -96,7 +175,7 @@ def build_dag(dag_id, schedule_interval='@daily', extra_default_args=None, extra
                 dest='bq://{project_id}:{pipeline_dataset}.{messages_table}'.format(**config),
                 segments='bq://{project_id}:{pipeline_dataset}.{segments_table}'.format(**config),
                 temp_shards_per_day="200",
-                runner='{dataflow_runner}.format(**config)',
+                runner='{dataflow_runner}'.format(**config),
                 project=config['project_id'],
                 disk_size_gb="50",
                 max_num_workers="100",
@@ -108,6 +187,34 @@ def build_dag(dag_id, schedule_interval='@daily', extra_default_args=None, extra
                 experiments='shuffle_mode=service'
             )
         )
+
+        # segment = DataFlowPythonOperator(
+        #     task_id='segment',
+        #     pool='dataflow',
+        #     depends_on_past=True,
+        #     py_file=Variable.get('DATAFLOW_WRAPPER_STUB'),
+        #     priority_weight=10,
+        #     command='{docker_run} {docker_image} segment'.format(**config),
+        #     startup_log_file=pp.join(Variable.get('DATAFLOW_WRAPPER_LOG_PATH'), 'pipe_segment/segment.log'),
+        #     options=dict(
+        #         date_range=date_range,
+        #         source=','.join(source_paths),
+        #         dest='bq://{project_id}:{pipeline_dataset}.{messages_table}'.format(**config),
+        #         segments='bq://{project_id}:{pipeline_dataset}.{segments_table}'.format(**config),
+        #         temp_shards_per_day="200",
+        #         runner='{dataflow_runner}'.format(**config),
+        #         project=config['project_id'],
+        #         disk_size_gb="50",
+        #         max_num_workers="100",
+        #         worker_machine_type='n1-highmem-2',
+        #         temp_location='gs://{temp_bucket}/dataflow_temp'.format(**config),
+        #         staging_location='gs://{temp_bucket}/dataflow_staging'.format(**config),
+        #         requirements_file='./requirements.txt',
+        #         setup_file='./setup.py',
+        #         experiments='shuffle_mode=service'
+        #     )
+        # )
+
 
         identity_messages_monthly = BashOperator(
             task_id='identity_messages_monthly',
