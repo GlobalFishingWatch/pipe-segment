979a961410438fe9040b229b1f0516efbf0b37e8
Paul Woods <pwoods25443@gmail.com>
2018-08-26 21:08:16 -0400

    Add segment_vessel

---
+++ scripts/segment_vessel.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+source pipe-tools-utils
+
+THIS_SCRIPT_DIR="$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )"
+
+ASSETS=${THIS_SCRIPT_DIR}/../assets
+source ${THIS_SCRIPT_DIR}/pipeline.sh
+
+PROCESS=$(basename $0 .sh)
+ARGS=( SEGMENT_VESSEL_TABLE DEST_TABLE )
+SCHEMA=${ASSETS}/${PROCESS}.schema.json
+SQL=${ASSETS}/${PROCESS}.sql.j2
+TABLE_DESC=(
+  "Many to many mapping between seg_id, vessel_id and ssvid"
+  ""
+  "* Pipeline: ${PIPELINE} ${PIPELINE_VERSION}"
+  "* Source: ${SEGMENT_VESSEL_TABLE}"
+  "* Command: $(basename $0)"
+)
+
+display_usage()
+{
+  echo -e "\nUsage:\n${PROCESS}.sh ${ARGS[*]} \n"
+}
+
+if [[ $# -ne ${#ARGS[@]} ]]
+then
+    display_usage
+    exit 1
+fi
+
+ARG_VALUES=("$@")
+PARAMS=()
+for index in ${!ARGS[*]}; do
+  echo ${ARG_VALUES[$index]}
+  declare "${ARGS[$index]}"="${ARG_VALUES[$index]}"
+  PARAMS+=("${ARGS[$index]}=${ARG_VALUES[$index]}")
+done
+
+TABLE_DESC+=(${PARAMS[*]})
+TABLE_DESC=$( IFS=$'\n'; echo "${TABLE_DESC[*]}" )
+
+
+echo "Publishing ${PROCESS} to ${DEST_TABLE}..."
+echo ""
+echo "Table Description" | indent
+echo "${TABLE_DESC}" | indent
+echo ""
+echo "Executing query..." | indent
+jinja2 ${SQL} \
+   -D segment_vessel_daily=${SEGMENT_VESSEL_TABLE//:/.} \
+   | bq query --headless --max_rows=0 --allow_large_results --replace \
+     --destination_table ${DEST_TABLE}  | indent
+
+echo ""
+bq update --schema ${SCHEMA} --description "${TABLE_DESC}" ${DEST_TABLE} | indent
+
+echo ""
+echo "DONE ${DEST_TABLE}."
