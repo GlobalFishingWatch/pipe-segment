04a411c382b6c4eba3c353a5e1ec0e27d447eaac
Enrique Tuya <enriquetuya@gmail.com>
2019-03-11 15:27:21 -0300

    Fixes issue running segmenter in dataflow due to a library update in gpsdio.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ DEPENDENCIES = [
     "newlinejson",
     "python-stdnum",
     "pipe-tools==0.2.5",
-    "gpsdio-segment==0.16",
+    "gpsdio-segment==0.17",
     "shipdataprocess==0.5.8",
     "jinja2-cli",
 ]
