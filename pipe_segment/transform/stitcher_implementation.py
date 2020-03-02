3dab36322339d498a723e6a50a8f6f4276010454
bitsofbits <tim@bitsofbits.com>
2020-03-02 14:58:48 -0700

    clean up and remove unused parameter to create tracks

--- pipe_segment/transform/stitcher_implementation.py
+++ pipe_segment/transform/stitcher_implementation.py
@@ -136,17 +136,9 @@ class StitcherImplementation(object):
         initial_tracks = list(self.reconstitute_tracks(tracks, segments))
         # Assure segments sorted to simplify pruning
         segments= sorted(segments, key=lambda x: x.timestamp)
-
-        # For now we are using pulling all segments in to reconstitute tracks
-        # in the future it might makes sense performance wise to only pull in 
-        # a finite time range of segments. To make that work, however, would
-        # require that we embed the final segment record in the track.
-        # track_sigs = stitcher.find_track_signatures(start_datetime, initial_tracks, segments)
-        track_sigs = {} # TODO: we need to save track and then build them as we go.
-
-        track_iter = stitcher.create_tracks(start_datetime, initial_tracks, track_sigs, segments)
-
         initial_track_ids = {track.id for track in initial_tracks}
+
+        track_iter = stitcher.create_tracks(start_datetime, initial_tracks, segments)
 
         # Condense tracks
         raw_tracks = []
@@ -186,47 +178,4 @@ class StitcherImplementation(object):
                            }
                 emit_datetime += DT.timedelta(days=1)
 
-
-
-
-        # while start_date <= self.end_date:
-        #     end_date = start_date + DT.timedelta(days=self.look_ahead)
-        #     # 
-        #     pruned_tracks = []
-        #     for track in raw_tracks:
-        #         new_track = [x for x in track if x['timestamp'].date() < start_date]
-        #         if new_track:
-        #             pruned_tracks.append(new_track)
-        #     # # Prune segments
-        #     track_sigs = stitcher.find_track_signatures(start_date, pruned_tracks, segments)
-        #     # for i, seg in enumerate(segments):
-        #     #     if seg['timestamp'].date() >= start_date:
-        #     #         break
-        #     # segments = segments[i:]
-        #     # Only keep segments up to lookahead forward
-        #     first_seg_date = start_date - DT.timedelta(MAX_LOOKBACK)
-        #     for i, seg in enumerate(segments):
-        #         if seg['timestamp'].date() >= first_seg_date:
-        #             break
-        #     else:
-        #         i += 1
-        #     segments = segments[i:]
-        #     for i, seg in enumerate(segments):
-        #         if seg['timestamp'].date() > end_date:
-        #             break
-        #     else:
-        #         i += 1
-        #     #
-        #     track_iter = stitcher.create_tracks(start_date, pruned_tracks, track_sigs, segments[:i])
-        #     # Condense tracks
-        #     raw_tracks = []
-        #     for raw_track, ndx, status in track_iter:
-        #         track = [x['aug_seg_id'] for x in raw_track]
-        #         track_id = track[0]
-        #         yield {'ssvid' : ssvid, 'track_id' : track_id, 'index': ndx,
-        #                'timestamp' : self._as_datetime(start_date), 'seg_ids' : track}
-        #         # TODO: status should be added to stored tracks and dealt with on input
-        #         if status == 'active':
-        #             raw_tracks.append(raw_track)
-        #     start_date += DT.timedelta(days=1)
 
