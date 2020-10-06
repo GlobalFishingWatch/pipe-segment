4ecde55b361139aa3d291e6c4a4186f571e5d812
Matias Piano <matias@globalfishingwatch.org>
2020-10-06 11:19:08 -0300

    Updates the version in __init__.py and add the entry in docker-compose for run test in travis

--- pipe_segment/__init__.py
+++ pipe_segment/__init__.py
@@ -3,7 +3,7 @@ Tools for parsing and normalizing AIS from Orbcomm using dataflow
 """
 
 
-__version__ = '3.0.5'
+__version__ = '3.0.6'
 __author__ = 'Paul Woods'
 __email__ = 'paul@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/pipe-segment'
