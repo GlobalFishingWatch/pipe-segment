f71a1ebd9c1b36eb58988b63177f5d92301d7574
Matias Piano <smpiano@gmail.com>
2022-04-18 10:04:13 -0300

    Sets the version in the pipe_segment package

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name='segment',
+    name='pipe_segment',
+    version=__import__('pipe_segment').__version__,
     packages=find_packages(exclude=['test*.*', 'tests']),
 )
