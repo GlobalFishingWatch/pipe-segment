d6ef7cb7f1e61b247ec5c61a2f5f8963f2192018
Matias Piano <smpiano@gmail.com>
2022-08-03 01:30:10 -0300

    Fixed write of the segment_identity_daily_ table when is in backfill mode

--- pipe_segment/segment_identity/pipeline.py
+++ pipe_segment/segment_identity/pipeline.py
@@ -290,11 +290,12 @@ class SegmentIdentityPipeline:
 
     @property
     def dest_segment_identity(self):
-        from_ts, _ = self.date_range
+        from_ts, to_ts = self.date_range
         return write_sink(
             self.options.dest_segment_identity,
             self.dest_segment_identity_schema,
             timezoneToDatetime(from_ts),
+            timezoneToDatetime(to_ts),
             "Daily segments identity processed in segment step.",
         )
 
