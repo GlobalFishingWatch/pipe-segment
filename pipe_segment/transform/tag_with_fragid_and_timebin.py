115abaea060167707da2cb2578725caf6eebcf84
Tim Hochberg <bitsofbits@users.noreply.github.com>
2022-09-27 20:16:50 -0700

    generate many fewer extraneous keys in TagWithFragIdAndTimeBin

--- pipe_segment/transform/tag_with_fragid_and_timebin.py
+++ pipe_segment/transform/tag_with_fragid_and_timebin.py
@@ -1,5 +1,5 @@
 import logging
-from datetime import date, timedelta
+from datetime import date
 
 from apache_beam import FlatMap, PTransform
 
@@ -14,11 +14,14 @@ class TagWithFragIdAndTimeBin(PTransform):
         self.bins_per_day = bins_per_day
 
     def tag_frags(self, x):
-        date = self.start_date
-        while date <= self.end_date:
+        frag_id = x["frag_id"]
+        date_str = frag_id.split("T")[0].split("-", 1)[1]
+        assert len(date_str) == 10
+        start_date_str = f"{self.start_date:%Y-%m-%d}"
+        end_date_str = f"{self.end_date:%Y-%m-%d}"
+        if start_date_str <= date_str <= end_date_str:
             for bin in range(self.bins_per_day):
                 yield ((x["frag_id"], str(date), bin), x)
-            date += timedelta(days=1)
 
     def expand(self, xs):
         return xs | FlatMap(self.tag_frags)
