f472ae5ecc3da831da51e1220616659b0cd5bc2c
Rodrigo Fuentes <rodrigo.fuentes@globalfishingwatch.org>
2024-05-30 11:36:18 -0300

    Fix linting on bq.py

--- pipe_segment/utils/bq.py
+++ pipe_segment/utils/bq.py
@@ -1,5 +1,6 @@
 from google.cloud import bigquery
 
+
 def get_bq_table(project_id, dataset_id, table_id):
     client = bigquery.Client(project=project_id)
     dataset_ref = client.dataset(dataset_id)
