d837d29d39b0d8754a3ff593fc1e1bfa4cb14e03
Tomás Link <tomaslink@gmail.com>
2024-06-11 21:36:43 -0300

    Add unit test for filter_bad_satellite_times

--- tests/transform/test_filter_bad_satellite_times.py
+++ tests/transform/test_filter_bad_satellite_times.py
@@ -2,6 +2,9 @@ import pytest
 from datetime import datetime
 from pytz import UTC
 
+import apache_beam as beam
+from apache_beam.testing.test_pipeline import TestPipeline
+
 from pipe_segment.transform.filter_bad_satellite_times import FilterBadSatelliteTimes
 from pipe_segment.tools import timestampFromDatetime
 
@@ -133,3 +136,94 @@ def test_not_during_bad_hour(msg, expected):
 
     bad_hours = dict((x, x) for x in bad_hours_list)
     assert tx.not_during_bad_hour(msg, bad_hours) == expected
+
+
+def test_beam():
+    satellite_offsets = [
+        {
+            "receiver": "FM130",
+            "dt": -3549.536305759878,
+            "pings": 104,
+            "avg_distance_from_sat_km": None,
+            "med_dist_from_sat_km": None,
+            "hour": 1712912400.0,
+        },
+        {
+            "receiver": "FM130",
+            "dt": -3505.5649172769745,
+            "pings": 1240,
+            "avg_distance_from_sat_km": None,
+            "med_dist_from_sat_km": None,
+            "hour": 1712926800.0,
+        },
+        {
+            "receiver": "FM130",
+            "dt": -3400.2222971353985,
+            "pings": 4010,
+            "avg_distance_from_sat_km": None,
+            "med_dist_from_sat_km": None,
+            "hour": 1712930400.0,
+        },
+        {
+            "receiver": "FM130",
+            "dt": -3421.456625837142,
+            "pings": 5724,
+            "avg_distance_from_sat_km": None,
+            "med_dist_from_sat_km": None,
+            "hour": 1712941200.0,
+        },
+    ]
+
+    messages = [
+        {
+            "timestamp": 1712932069.0,
+            "msgid": "438dd53e-8c0c-a148-0acb-b59f29d0b88c",
+            "source": "exactearth",
+            "type": "AIS.27",
+            "ssvid": "226010660",
+            "lon": 4.84,
+            "lat": 45.76,
+            "speed": 0.0,
+            "course": None,
+            "heading": None,
+            "shipname": None,
+            "callsign": None,
+            "destination": None,
+            "imo": None,
+            "shiptype": None,
+            "receiver_type": "satellite",
+            "receiver": "1161",
+            "length": None,
+            "width": None,
+            "status": 0,
+            "class_b_cs_flag": None,
+        },
+        {
+            "timestamp": 1712931889.0,
+            "msgid": "55f94c90-95ba-6541-4ebb-c6788642c0f1",
+            "source": "exactearth",
+            "type": "AIS.27",
+            "ssvid": "226010660",
+            "lon": 4.84,
+            "lat": 45.763333333333335,
+            "speed": 6.0,
+            "course": None,
+            "heading": None,
+            "shipname": None,
+            "callsign": None,
+            "destination": None,
+            "imo": None,
+            "shiptype": None,
+            "receiver_type": "satellite",
+            "receiver": "1161",
+            "length": None,
+            "width": None,
+            "status": 0,
+            "class_b_cs_flag": None,
+        },
+    ]
+    with TestPipeline() as p:
+        input_pcoll = p | beam.Create(messages)
+        pcoll_sat_offsets = p | "CreateSatOffsets" >> beam.Create(satellite_offsets)
+
+        input_pcoll | FilterBadSatelliteTimes(pcoll_sat_offsets, 30, 1)
