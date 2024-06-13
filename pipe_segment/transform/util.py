d24c0ce66333f93e621c19eff9ed18f500e5972f
Tomás Link <tomaslink@gmail.com>
2024-06-13 19:35:00 -0300

    Add unit test for by_day

--- pipe_segment/transform/util.py
+++ pipe_segment/transform/util.py
@@ -1,17 +1,18 @@
-from ..tools import datetimeFromTimestamp
+from pipe_segment.tools import datetimeFromTimestamp
 
 
 def by_day(items, key="timestamp"):
     items = sorted(items, key=lambda x: x[key])
-    current = []
     day = datetimeFromTimestamp(items[0][key]).date()
+
+    current = []
     for x in items:
         new_day = datetimeFromTimestamp(x[key]).date()
         if new_day != day:
-            assert len(current) > 0
             yield day, current
+
             current = []
             day = new_day
         current.append(x)
-    assert len(current) > 0
+
     yield day, current
