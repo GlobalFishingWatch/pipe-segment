174c551edb1c12da7fb0ccd23dd172adc2a691b9
Tomás Link <tomaslink@gmail.com>
2024-06-14 00:17:31 -0300

    Add unit test for WhitelistFields

--- pipe_segment/transform/whitelist_messages_segmented.py
+++ pipe_segment/transform/whitelist_messages_segmented.py
@@ -1,13 +1,16 @@
 import apache_beam as beam
-from pipe_segment.message_schema import message_output_schema as mos
-
-fieldnames = list(map(lambda x: x['name'], mos['fields']))
+from pipe_segment import message_schema
 
 
 class WhitelistFields(beam.PTransform):
+    def __init__(self, fieldnames=None):
+        self._fieldnames = fieldnames
+
+        if self._fieldnames is None:
+            self._fieldnames = message_schema.fieldnames
 
     def whitelist(self, elem):
-        return {field: elem[field] for field in fieldnames if field in elem}
+        return {field: elem[field] for field in self._fieldnames if field in elem}
 
     def expand(self, xs):
         return xs | beam.Map(self.whitelist)
