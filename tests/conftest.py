a7c7c3a6e94083a15e39188c329a591689da9c9c
Tomás Link <tomaslink@gmail.com>
2024-06-18 17:35:38 -0300

    Remove temp_dir fixture. Use tmp_path built-in fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,5 @@
-import posixpath
 import os
-import tempfile
-import shutil
-
+import posixpath
 import pytest
 
 
@@ -30,22 +27,7 @@ def pytest_collection_modifyitems(config, items):
 TESTS_DIR = os.path.dirname(os.path.realpath(__file__))
 TEST_DATA_DIR = posixpath.join(TESTS_DIR, 'data')
 
-# NB:  This module is magically imported when you run py.test
-# and the fixtures below are magically provided to any test function in any test module
-# without needing to import them of declare them
-
 
 @pytest.fixture(scope='session')
 def test_data_dir():
     return TEST_DATA_DIR
-
-
-@pytest.fixture(scope='function')
-def temp_dir(request):
-    d = tempfile.mkdtemp()
-
-    def fin():
-        shutil.rmtree(d, ignore_errors=True)
-
-    request.addfinalizer(fin)
-    return d
