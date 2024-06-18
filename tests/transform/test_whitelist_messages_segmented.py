c50dd6647ff35a6d3fd9bf5b4069c76060a7c9c3
Tomás Link <tomaslink@gmail.com>
2024-06-18 17:22:37 -0300

    Put schemas on their own folder

--- tests/transform/test_whitelist_messages_segmented.py
+++ tests/transform/test_whitelist_messages_segmented.py
@@ -3,7 +3,7 @@ from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 from apache_beam.testing.util import equal_to
 
-from pipe_segment import message_schema
+from pipe_segment.schemas import message_schema
 from pipe_segment.transform.whitelist_messages_segmented import WhitelistFields
 
 
