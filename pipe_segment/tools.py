020f11cff0833d3fffa8e41991bc5812175156c9
Tomás Link <tomaslink@gmail.com>
2024-06-19 12:36:48 -0300

    Add unit test for tools.py

--- pipe_segment/tools.py
+++ pipe_segment/tools.py
@@ -1,20 +1,23 @@
 # import datetime
-from datetime import timezone, date, datetime
+from datetime import date, datetime, timezone as tz
 
 import six
 from dateutil.parser import parse as dateutil_parse
 
-EPOCH = datetime.utcfromtimestamp(0).replace(tzinfo=timezone.utc)
+EPOCH = datetime.utcfromtimestamp(0).replace(tzinfo=tz.utc)
 
 
 def timestamp_from_datetime(dt):
     """Convert a datetime to a unix timestamp"""
+    if dt.tzinfo is None:
+        dt = dt.replace(tzinfo=tz.utc)
+
     return (dt - EPOCH).total_seconds()
 
 
 def datetime_from_timestamp(ts):
     """Convert a unix timestamp to a datetime"""
-    return datetime.fromtimestamp(ts, tz=timezone.utc)
+    return datetime.fromtimestamp(ts, tz=tz.utc)
 
 
 def as_timestamp(d):
@@ -27,16 +30,18 @@ def as_timestamp(d):
     """
     if d is None:
         return None
-    elif isinstance(d, datetime):
-        return timestamp_from_datetime(d.replace(tzinfo=timezone.utc))
-    elif isinstance(d, date):
+
+    if isinstance(d, datetime):
+        return timestamp_from_datetime(d.replace(tzinfo=tz.utc))
+
+    if isinstance(d, date):
         return timestamp_from_datetime(
-            datetime.combine(d, datetime.min.time()).replace(tzinfo=timezone.utc))
-    elif isinstance(d, (float, int)):
+            datetime.combine(d, datetime.min.time()).replace(tzinfo=tz.utc))
+
+    if isinstance(d, (float, int)):
         return float(d)
-    elif isinstance(d, six.string_types):
+
+    if isinstance(d, six.string_types):
         return as_timestamp(dateutil_parse(d))
-    else:
-        raise ValueError(
-            'Unsupported data type. Unable to convert value "%s" to to a timestamp' % d
-        )
+
+    raise ValueError(f'Unsupported data type. Unable to convert value "{d}" to to a timestamp')
