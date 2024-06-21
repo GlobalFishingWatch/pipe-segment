9b97baebd8319645ed2f46b73b648e1a99d45c16
Tomás Link <tomaslink@gmail.com>
2024-06-21 13:45:00 -0300

    Move list_of_days to utility module and fix bug

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -24,3 +24,17 @@ def test_timestamp_from_string():
     ts = 1704067200.0
 
     assert tools.timestamp_from_string("2024-01-01") == ts
+
+
+def test_list_of_days():
+    start_date = "2024-01-01"
+    end_date = "2024-01-05"
+    expected = ['2024-01-01', '2024-01-02', '2024-01-03', '2024-01-04']
+
+    start_dt = tools.datetime_from_string(start_date)
+    end_dt = tools.datetime_from_string(end_date)
+
+    days = tools.list_of_days(start_dt, end_dt)
+    days_iso = [x.date().isoformat() for x in list(days)]
+
+    assert days_iso == expected
