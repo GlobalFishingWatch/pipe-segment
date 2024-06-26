416be49f70f0263c607d075a0ca952d38757c264
Tomás Link <tomaslink@gmail.com>
2024-06-26 13:33:30 -0300

    Remove bq.py, reuse BigQueryTools to perform queries.

--- pipe_segment/models/bigquery_message_source.py
+++ pipe_segment/models/bigquery_message_source.py
@@ -3,27 +3,36 @@ from datetime import datetime
 
 from jinja2 import Template
 
-from pipe_segment.utils.bq import get_bq_table
+from pipe_segment.utils.bqtools import BigQueryTools
 
 
 class BigQueryMessagesSource:
+    DEFAULT_FILTER_TEMPLATE = """
+        DATE({{ filter_field }})
+        BETWEEN '{{ start_date.strftime(date_format) }}'
+        AND '{{ end_date.strftime(date_format) }}'
+    """
 
     def __init__(
         self,
+        bqtools: BigQueryTools,
         table_id: str,
-        filter_template: str = None
+        filter_template: str = DEFAULT_FILTER_TEMPLATE
     ):
-        self.table_id = table_id
-        self.filter_template = filter_template
+        self._bqtools = bqtools
+        self._table_id = table_id
+        self._filter_template = filter_template
+        self._check_sharded_or_partitioned(table_id)
 
-        # Checks whether the provided table is date sharded or partitioned
+    def _check_sharded_or_partitioned(self, table_id):
+        # TODO: clean a little bit this code.
         try:
             project_id, dataset_id, table_name = table_id.replace("bq://", "") \
                 .replace(":", ".") \
                 .split(".")
             self._table_suffix = ''
             # When the table is found as provided in the arguments is partitioned
-            self._table = get_bq_table(project_id, dataset_id, table_name)
+            self._table = self._bqtools.get_table(dataset_id, table_name)
 
             if self._table.time_partitioning:
                 partitioning_field = self._table.time_partitioning.field
@@ -41,8 +50,8 @@ class BigQueryMessagesSource:
                 # Ensure the table is sharded
                 self._table_suffix = '*'
                 try:
-                    self._table = get_bq_table(project_id, dataset_id,
-                                               f"{table_name}{self._table_suffix}")
+                    self._table = self._bqtools.get_table(
+                        dataset_id, f"{table_name}{self._table_suffix}")
                     self._filtering_field = "_TABLE_SUFFIX"
                     self._date_format = "%Y%m%d"
                     logging.info(f'Table {table_id}* is date sharded')
@@ -54,19 +63,13 @@ class BigQueryMessagesSource:
                 raise e
 
     def filter_messages(self, start_date: datetime, end_date: datetime) -> str:
-        template = self.filter_template
-        if template is None:
-            template = "DATE({{ filter_field }}) " + \
-                "BETWEEN '{{ start_date.strftime(date_format) }}' " + \
-                " AND '{{ end_date.strftime(date_format) }}'"
-
-        filter = Template(template).render({'start_date': start_date,
-                                            'end_date': end_date,
-                                            'filter_field': self._filtering_field,
-                                            'date_format': self._date_format,
-                                            })
-        return filter
+        return Template(self._filter_template).render({
+            'start_date': start_date,
+            'end_date': end_date,
+            'filter_field': self._filtering_field,
+            'date_format': self._date_format,
+        })
 
     @property
     def qualified_source_messages(self) -> str:
-        return f"{self.table_id}{self._table_suffix}"
+        return f"{self._table_id}{self._table_suffix}"
