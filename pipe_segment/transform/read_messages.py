1c66418726807f138a808665cfba676b6ec717d5
Tomás Link <tomaslink@gmail.com>
2024-06-26 17:57:35 -0300

    Use BigQueryMessagesSource directly inside ReadMessages

--- pipe_segment/transform/read_messages.py
+++ pipe_segment/transform/read_messages.py
@@ -1,5 +1,6 @@
 import logging
-from typing import List
+
+from jinja2 import Template
 
 import apache_beam as beam
 from pipe_segment.models.bigquery_message_source import BigQueryMessagesSource
@@ -7,29 +8,39 @@ from pipe_segment.models.bigquery_message_source import BigQueryMessagesSource
 
 logger = logging.getLogger(__name__)
 
+FILTER_TEMPLATE = """
+    DATE({{ filter_field }})
+    BETWEEN '{{ start_date.strftime(date_format) }}'
+    AND '{{ end_date.strftime(date_format) }}'
+"""
+
 
 class ReadMessages(beam.PTransform):
     def __init__(
         self,
+        bqtools,
         sources,
         start_date,
         end_date,
         ssvid_filter_query=None,
     ):
-        self.sources: List[BigQueryMessagesSource] = sources
+        self.bqtools = bqtools
+        self.sources = sources
         self.start_date = start_date
         self.end_date = end_date
         self.ssvid_filter_query = ssvid_filter_query
 
-    def query(self, source: BigQueryMessagesSource):
+    def query(self, source: str):
+        bq_source = BigQueryMessagesSource(self.bqtools, source)
+
         query = f"""
         SELECT *
         FROM (
             SELECT
               CAST(UNIX_MICROS(timestamp) AS FLOAT64) / 1000000  AS timestamp,
                 * except (timestamp)
-            FROM `{source.qualified_source_messages}`
-            WHERE {source.filter_messages(start_date=self.start_date, end_date=self.end_date)}
+            FROM `{bq_source.qualified_source_messages}`
+            WHERE {self._build_filter(bq_source)}
         )
         """
         if self.ssvid_filter_query is not None:
@@ -38,6 +49,14 @@ class ReadMessages(beam.PTransform):
         logger.debug(f"QUERY:\n{query}")
 
         return query
+
+    def _build_filter(self, source: BigQueryMessagesSource):
+        return Template(FILTER_TEMPLATE).render({
+            'start_date': self.start_date,
+            'end_date': self.end_date,
+            'filter_field': source.filtering_field,
+            'date_format': source.date_format,
+        })
 
     def expand(self, pcoll):
         return [
