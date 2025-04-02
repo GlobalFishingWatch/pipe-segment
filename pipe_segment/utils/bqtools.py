98e6d83152be380480d6ccc14b741eb84aeccac9
Matias Piano <matias@globalfishingwatch.org>
2025-04-02 11:11:44 -0300

    Makes remove of data content generic and places it and get_bq_table in only one place

--- pipe_segment/utils/bqtools.py
+++ pipe_segment/utils/bqtools.py
@@ -1,4 +1,4 @@
-import logging
+import logging, functools
 from datetime import timedelta
 
 from google.cloud import bigquery
@@ -17,53 +17,67 @@ class BigQueryTools():
     def __init__(self, project):
         self.client = bigquery.Client(project=project)
 
+    def get_bq_table(self, dataset_id, table_id):
+        dataset_ref = self.client.dataset(dataset_id)
+        table_ref = dataset_ref.table(table_id)
+        return self.client.get_table(table_ref)
+
     def ensure_table_exists(self, table):
         logging.info(f"Ensuring table {table.table_id} exists")
         result = self.client.create_table(table=table, exists_ok=True)
         logging.info(f"Table {result.full_table_id} exists")
         return result
 
-    def clear_records(self, table, date_field, date_from, date_to):
-        logging.info(
-            f"""Deleting records at
-            table {table.full_table_id} between {date_from:%Y-%m-%d} and {date_to:%Y-%m-%d}""")
-        query_job = self.client.query(
-            f"""
-               DELETE FROM `{table.project}.{table.dataset_id}.{table.table_id}`
-               WHERE DATE({date_field}) BETWEEN '{date_from:%Y-%m-%d}' AND '{date_to:%Y-%m-%d}'
-            """,
-            bigquery.QueryJobConfig(
-                use_legacy_sql=False,
-            )
+    def remove_content(
+            table: str,
+            date_range: str,
+            labels: list = [],
+            partition_field: str = "timestamp"
+    ):
+        """Removes the data content from a table to specific date range.
+        Useful when we need to re-process a time period with fresh data.
+
+        :param table: Table path Format: <dataset.table>.
+        :param date_range: Date range to clean the table content.
+        :param labels: Labels to audit the operation.
+        :param partition_field: Partition field of the table.
+        """
+        logging.info(f'Removing the data content of [{table}] period <{date_range}>.')
+        table_ds, table_tb = table.split('.')
+        table_ref = self.get_bq_table(table_ds, table_tb)
+        date_from, date_to = list(map(lambda x:x.strip(), date_range.split(',')))
+        labels = functools.reduce(
+            lambda x,y: dict(x, **{y.split('=')[0]: y.split('=')[1]}),
+            labels,
+            dict()
         )
-        result = query_job.result()
-        logging.info(
-            f"""Records at table {table.full_table_id} between
-            {date_from:%Y-%m-%d} and {date_to:%Y-%m-%d} deleted""")
-        return result
 
-    def ensure_sharded_tables_creation(
-            self,
-            start_date,
-            end_date,
-            destination_tables,
-            key="timestamp"):
-        # Ensure sharded tables are created for all dates
-        logging.info("Ensure sharded tables are created for all dates")
-
-        def full_tbl_id(tbl): return f"{self.client.project}.{tbl}" if not (
-            self.client.project in tbl) else tbl
-
-        # Create list of days between start_date and end_date including the start_date.
-        days = ([start_date] + [start_date + timedelta(days=x)
-                for x in range((end_date - start_date).days)])
-        for dt in days:
-
-            for dst in destination_tables.keys():
-                sink_table_descriptor = build_sink_table_descriptor(
-                    table_id=f"{full_tbl_id(destination_tables[dst]['table'])}{dt:%Y%m%d}",
-                    schema=destination_tables[dst]["schema"]["fields"],
-                    description=destination_tables[dst]["description"]
+        try:
+            table = self.client.get_table(table_ref) #API request
+            logging.info(f'Removing the data content: Ensures the table [{table}] exists.')
+            #deletes the content
+            query_job = self.client.query(
+                f"""
+                   DELETE FROM `{table}`
+                   WHERE date({partition_field}) >= '{date_from}'
+                   AND date({partition_field}) <= '{date_to}'
+                """, # incusive range
+                bigquery.QueryJobConfig(
+                    use_query_cache=False,
+                    use_legacy_sql=False,
+                    labels=labels,
                 )
-                sink_table = self.ensure_table_exists(sink_table_descriptor)
-                self.clear_records(sink_table, key, dt, dt)
+            )
+            logging.info(f'Removing the data content: Job {query_job.job_id},'
+                         f' is currently in state {query_job.state}')
+            result = query_job.result()
+            logging.info(f'Removing the data content: Date range '
+                         f'[{date_from},{date_to}] cleaned in table {table}: {result}')
+
+        except NotFound as nferr:
+            logging.warn(f'Removing the data content: Table {table} NOT FOUND. We can go on.')
+
+        except BadRequest as err:
+            logging.error(f'Removing the data content: Bad request received {err}.')
+
+        return result
