3df2141193b651681a6ece5952f7299fb48c9ccc
Matias Piano <matias@globalfishingwatch.org>
2025-04-22 12:07:29 -0300

    Fixing changes in tests

--- tests/transform/test_satellite_offsets.py
+++ tests/transform/test_satellite_offsets.py
@@ -6,8 +6,7 @@ from google.cloud import bigquery
 from google.api_core.exceptions import NotFound, BadRequest
 
 
-from pipe_segment.transform.satellite_offsets import SatelliteOffsets, SatelliteOffsetsWrite
-from pipe_segment.transform.satellite_offsets import remove_satellite_offsets_content
+from pipe_segment.transform.satellite_offsets import SatelliteOffsets
 
 from apache_beam.testing.test_pipeline import TestPipeline
 
@@ -15,15 +14,6 @@ from apache_beam.testing.test_pipeline import TestPipeline
 class ReadFromBigQueryMock(beam.io.ReadFromBigQuery):
     def expand(self, pcoll):
         return pcoll | beam.Create(["dummy"])
-
-
-class WriteToBigQueryMock(beam.io.WriteToBigQuery):
-    def __init__(self, table, *args, **kwargs):
-        self._table = table
-        super().__init__(table, *args, **kwargs)
-
-    def expand(self, pcoll):
-        return pcoll | beam.Map(self._table)
 
 
 class RowMock:
@@ -88,36 +78,14 @@ class CloudOptionsMock:
 def test_satellite_offsets(monkeypatch):
     # TODO: replace this monkey patch when design allows for more easy testing.
     monkeypatch.setattr(beam.io, "ReadFromBigQuery", ReadFromBigQueryMock)
-    monkeypatch.setattr(beam.io, "WriteToBigQuery", ReadFromBigQueryMock)
-
-    remove_sat_offsets_config = dict(
-        destination_table=OptionsMock.out_sat_offsets_table,
-        date_range=OptionsMock.date_range,
-        project=CloudOptionsMock.project,
-        labels_list=CloudOptionsMock.labels
-    )
-
-    monkeypatch.setattr(bigquery, "Client", lambda *x, **y: BigQueryClientMock(notfound=True))
-    remove_satellite_offsets_content(**remove_sat_offsets_config)
-
-    monkeypatch.setattr(bigquery, "Client", lambda *x, **y: BigQueryClientMock(badrequest=True))
-    remove_satellite_offsets_content(**remove_sat_offsets_config)
-
-    monkeypatch.setattr(bigquery, "Client", BigQueryClientMock)
-    remove_satellite_offsets_content(**remove_sat_offsets_config)
 
     dummy_table = ""
     start_date = datetime(2024, 1, 1)
     end_date = datetime(2024, 1, 1)
 
     query_op = SatelliteOffsets(dummy_table, dummy_table, dummy_table, start_date, end_date)
-    write_op = SatelliteOffsetsWrite(OptionsMock(), CloudOptionsMock())
 
     # messages = []
     with TestPipeline() as p:
 
         p | query_op
-        p | write_op
-
-        write_op.update_table_description()
-        write_op.update_labels()
