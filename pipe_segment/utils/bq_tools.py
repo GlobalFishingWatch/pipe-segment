8de5f8be21ddc669f676234ad6abf2d1b511e2a2
Matias Piano <matias@globalfishingwatch.org>
2025-05-14 17:31:28 -0300

    Removes commented code and ident the code in order to PEP8

--- pipe_segment/utils/bq_tools.py
+++ pipe_segment/utils/bq_tools.py
@@ -35,62 +35,6 @@ class BigQueryTools:
         """
         return cls(bigquery.Client(project=project, **kwargs))
 
-    # def create_or_clear_tables(self, tables, start_date, end_date=None, date_field="timestamp"):
-    #     """Creates sharded tables. If they already exist, deletes their records.
-
-    #     Args:
-    #         tables: list of tables.
-    #         start_date: the start of dates interval.
-    #         end_date: the end of dates interval.
-    #         date_field: name of the field with datetime.
-    #     """
-    #     logging.info("Ensure sharded tables are created for all dates...")
-    #     if end_date is None or end_date == start_date:
-    #         end_date = start_date + timedelta(days=1)
-
-    #     for date in tools.list_of_days(start_date, end_date):
-    #         for table_config in tables.values():
-    #             table_ref, table_config = self._resolve_table_id(date, **table_config)
-    #             table = self.create_table(table_ref, **table_config)
-    #             self.clear_records(table, date_field, date, date)
-
-    # def create_table(self, table_ref: str, **table_config) -> bigquery.Table:
-    #     logging.info(f"Creating table {table_ref} (if it doesn't exists yet)...")
-    #     table = self.build_table(table_ref=table_ref, **table_config)
-    #     return self.client.create_table(table=table, exists_ok=True)
-
-    # def clear_records(self, table, date_field, date_from, date_to):
-    #     logging.info(
-    #         "Deleting records at table {} between {} and {}..."
-    #         .format(table.full_table_id, date_from, date_to)
-    #     )
-
-    #     full_table_id = table.full_table_id.replace(":", ".")
-
-    #     query = DELETE_BETWEEN_DATES_QUERY.format(
-    #         table=full_table_id, field=date_field, start=date_from, end=date_to)
-
-    #     query_job = self.client.query(query, bigquery.QueryJobConfig(use_legacy_sql=False))
-
-    #     return query_job.result()
-
-    # def _resolve_table_id(self, date, table, **rest_of_config):
-    #     if self.client.project not in table:
-    #         table = f"{self.client.project}.{table}"
-
-    #     table = f"{table}{date:%Y%m%d}"
-
-    #     return table, rest_of_config
-
-    # @staticmethod
-    # def build_table(table_ref, schema, description):
-    #     table = bigquery.Table(
-    #         table_ref=table_ref.replace("bq://", "").replace(":", "."),
-    #         schema=schema["fields"],
-    #     )
-    #     table.description = description
-
-    #     return table
     def get_bq_table(self, dataset_id: str, table_id: str) -> bigquery.Table:
         """Returns the bigquery.Table from the dataset and table ids.
 
@@ -131,13 +75,11 @@ class BigQueryTools:
             table = self.client.get_table(table_ref)  # API request
             logger.info(f'Removing the data content: Ensures the table [{table}] exists.')
             # deletes the content
-            query_job = self.client.query(DELETE_BETWEEN_DATES_QUERY,
-                bigquery.QueryJobConfig(
-                    use_query_cache=False,
-                    use_legacy_sql=False,
-                    labels=labels,
-                )
-            )
+            query_job = self.client.query(DELETE_BETWEEN_DATES_QUERY, bigquery.QueryJobConfig(
+                use_query_cache=False,
+                use_legacy_sql=False,
+                labels=labels,
+            ))
             logger.info(f'Removing the data content: Job {query_job.job_id},'
                         f' is currently in state {query_job.state}')
             result = query_job.result()
