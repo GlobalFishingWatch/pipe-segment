6afc9b3dae74dd49138d9b30ea396f953ce35f68
Matias Piano <matias@globalfishingwatch.org>
2025-06-26 10:52:29 -0300

    Support in/out in different project and datasets of segment tables

--- pipe_segment/migration.py
+++ pipe_segment/migration.py
@@ -23,47 +23,42 @@ logger = logging.getLogger(__name__)
 
 NAME = "migration-script"
 DESCRIPTION = "Use to migrate the segment tables from date-sharded to date-partition."
-PROJECT = "world-fishing-827"  # project to bill and where the tables exists.
-DATASET_OUT = "scratch_matias_ttl7d"  # dataset to create the date-partitioned tables
-DATASET_SOURCE = "pipe_ais_v3_internal"  # dataset where to query the date-sharded tables
-DATE_START = "2012-01-01"
 QUERY = """
 SELECT * FROM `{table_id}_*`
-WHERE _TABLE_SUFFIX BETWEEN '{start}' AND '{end}'
 """
 
 
 def define_tables(dataset_out):
     messages_segmented = DatePartitionedTable(
-        table_id=f"{PROJECT}.{dataset_out}.messages_segmented",
+        table_id=f"{dataset_out}.messages_segmented",
         schema=message_schema.message_output_schema,
         description=f"Created by pipe-segment:{__version__}.\n"
                     "Daily satellite messages segmented processed in segment step.",
         partitioning_field="timestamp",
     )
     segments = DatePartitionedTable(
-        table_id=f"{PROJECT}.{dataset_out}.segments",
+        table_id=f"{dataset_out}.segments",
         schema=segment_schema.segment_schema,
         description=f"Created by pipe-segment:{__version__}.\n"
                     "Daily segments processed in segment step.",
         partitioning_field="timestamp",
     )
     fragments = DatePartitionedTable(
-        table_id=f"{PROJECT}.{dataset_out}.fragments",
+        table_id=f"{dataset_out}.fragments",
         schema=Fragment.schema,
         description=f"Created by pipe-segment:{__version__}.\n"
                     "Daily fragments processed in segment step.",
         partitioning_field="timestamp",
     )
     segment_identity_daily = DatePartitionedTable(
-        table_id=f"{PROJECT}.{dataset_out}.segment_identity_daily",
+        table_id=f"{dataset_out}.segment_identity_daily",
         description=f"Created by the pipe-segment: {__version__}.\n"
                     "Daily segments identity processed in segment step.",
         schema=DEST_SEGMENT_IDENTITY_SCHEMA,
         partitioning_field="summary_timestamp",
     )
     segment_vessel_daily = DatePartitionedTable(
-        table_id=f"{PROJECT}.{dataset_out}.segment_vessel_daily",
+        table_id=f"{dataset_out}.segment_vessel_daily",
         description=description("Migration"),
         schema=Schemas.load_json_schema(SCHEMA_SEGMENTVESSELDAILY_PATH),
         partitioning_field="day",
@@ -77,14 +72,12 @@ def define_tables(dataset_out):
     ]
 
 
-def prepare_output_tables(bq_helper, dataset_out, tables, start, end):
+def prepare_output_tables(bq_helper, dataset_in, dataset_out, tables):
     for table in tables:
         bq_helper.ensure_table_exists(table)
         logging.info(f"Ensures table {table.table_id} is created.")
         query = QUERY.format(
-            table_id=table.table_id.replace(dataset_out, DATASET_SOURCE),
-            start=start,
-            end=end
+            table_id=table.table_id.replace(dataset_out, dataset_in),
         )
         logger.info(query)
         bq_helper.run_query_into_table(query=query, table=table)
@@ -93,10 +86,6 @@ def prepare_output_tables(bq_helper, dataset_out, tables, start, end):
 def migrate(args):
     # Definitions
     logging.getLogger().setLevel(logging.INFO)
-    date_start, date_end = [DATE_START, dt.datetime.now().date().isoformat()]
-
-    start = dt.datetime.fromisoformat(date_start)
-    end = dt.datetime.fromisoformat(date_end)
     bq_client = bigquery.Client(project=args.billing_project)
     labels = [
         "environment=develop",
@@ -108,16 +97,22 @@ def migrate(args):
     ]
     bq_helper = BigQueryHelper(bq_client, labels)
     tables = define_tables(args.dataset_out)
-    prepare_output_tables(bq_helper, args.dataset_out, tables,
-                          start.strftime('%Y%m%d'), end.strftime('%Y%m%d'))
+    prepare_output_tables(bq_helper, args.dataset_in, args.dataset_out, tables)
 
 
 def run(args) -> int:
     parser = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION)
     parser.add_argument(
+        '--dataset_in',
+        type=str,
+        help="Project.Dataset to read date-sharded tables from "
+             "(ex world-fishing-827.pipe_ais_v3_internal)."
+    )
+    parser.add_argument(
         '--dataset_out',
         type=str,
-        help="Dataset where to build the date-partitioned tables."
+        help="Project.Dataset where to build the date-partitioned tables "
+             "(ex world-fishing-827.pipe_ais_v3_internal)."
     )
     parser.add_argument(
         '--billing_project',
