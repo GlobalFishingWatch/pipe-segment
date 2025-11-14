f918b451a6aa1d36877d7be615d5f8f493a09c1b
Tomás Link <tomas.link@globalfishingwatch.org>
2025-11-14 14:12:20 -0300

    Adapt migration script for encounters

---
+++ examples/migration-sharded-tables.sh
@@ -0,0 +1,4 @@
+python ./pipe_segment/migration.py \
+  --dataset_in "gfw-int-pipe-v3.pipe_ais_v3_internal" \
+  --dataset_out "gfw-int-pipe-v3.pipe_ais_v3_internal" \
+  --billing_project "world-fishing-827"
